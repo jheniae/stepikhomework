48946d7f3a28b1455d6f1745454d3e62f1186f70
jheniae <jheniae@gmail.com>
2019-09-07 17:28:52 +0300

    install pytest

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,14 +1,15 @@
+import unittest
 
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+class TestAbs(unittest.TestCase):
+    def test_abs1():
+        self.assertEqual(abs(-42) == 42, "Should be absolute value of a number")
 
 
 
-def test_abs2():
-    assert abs(-42) == -42, "Should be absolute value of a number"
+    def test_abs2():
+        self.assertEqual(abs(-42) == -42, "Should be absolute value of a number")
 
 
 if __name__ == '__main__':
-    test_abs1()
-    test_abs2()
+    unittest.main()
     print('All tests passed')
